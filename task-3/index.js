c7402d671c56dc0dc377919ef03f50cd5fafd4bb
Yulia Nikolaeva <n7454@meta.ua>
2020-02-16 00:30:49 +0200

    HW-11-02

---
+++ task-3/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/*
+Задание 3
+Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции 
+onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
+*/
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3 ?
+        res([transaction.id, delay]) : rej(transaction.id);
+        return canProcess;
+    }, delay);
+  });
+};
+
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = (id) => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/* Должно работать так */
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
